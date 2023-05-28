4e0e6e1322cfed6b15fd394955f6746027746b86
Paulo Diego <paulo.diego@unip.br>
2023-05-28 13:49:15 -0300

    CRUD Angular inicial

--- src/app/models/candidato.ts
+++
@@ -1,8 +0,0 @@
-export interface Candidato {
-    id: number;
-    id_Grupo: number;
-    nome_Candidato: string;
-    cidade_Candidato: string;
-    pretensao_Candidato: number;
-    especialidade_Candidato: string;
-}
