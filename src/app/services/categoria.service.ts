e162e17d7d41626531af798a297e3eaa43407566
Paulo Diego <paulo.diego@unip.br>
2023-05-28 13:54:15 -0300

    Services

---
+++ src/app/services/categoria.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+]import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Categoria } from '../models/categoria';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriaService {
+
+  urlApi = 'https://sistemashomologacao.suafaculdade.com.br/ObjetivoGames/api/Categoria';
+
+  constructor(private http: HttpClient) { }
+
+  getElements(): Observable<Categoria[]> {
+    return this.http.get<Categoria[]>(this.urlApi);
+  }
+
+  createElements(element: Categoria): Observable<Categoria> {
+    return this.http.post<Categoria>(this.urlApi, element);
+  }
+
+  editElement(element: Categoria): Observable<Categoria> {
+    return this.http.put<Categoria>(this.urlApi, element);
+  }
+
+  deleteElement(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.urlApi}?id=${id}`);
+  }
+}
